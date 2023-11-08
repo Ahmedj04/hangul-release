a12262ba8335d07a36631a4b44d44a941e70c1a2
Ahmed <janahmed902@gmail.com>
2023-11-08 11:21:02 +0530

    resolved one import

--- components/BookingEngine/BookingSuccess.js
+++ components/BookingEngine/BookingSuccess.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { AiOutlineClose } from "react-icons/ai";
 
 import { useDispatch, useSelector } from 'react-redux';
-import { clearRoomsSelected, clearReservationIdentity, setAddMoreRoom, clearGuestDetails } from '../redux/hangulSlice';
+import { clearRoomsSelected, clearReservationIdentity, setAddMoreRoom, clearGuestDetails, clearInventoryDetail } from '../redux/hangulSlice';
 
 function BookingSuccess({ setDisplay, setShowModal, setSearched, rooms, checkinDate, checkoutDate }) {
 
